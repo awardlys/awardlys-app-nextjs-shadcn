9174fcb9882c51d3027228645a4e0eabc176ab3d
Rivaldo Mascarenhas <rivaldomascarenhas007@gmail.com>
2024-01-21 19:52:03 -0300

    refactor(services): remove logic to get localstorage

--- src/services/http/api.ts
+++ src/services/http/api.ts
@@ -1,11 +1,8 @@
 import axios from "axios";
 
-const token = JSON.parse(localStorage.getItem("token") ?? "null");
-
 export const api = axios.create({
   baseURL: "http://localhost:3001/api",
   headers: {
     "Content-Type": "application/json",
-    Authorization: "Bearer " + token,
   },
 });
